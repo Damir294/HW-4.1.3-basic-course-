8e85ab7dfb2e2f06f64e93382c15594e8e7689f6
Дамир <damir.obuhov@mail.ru>
2022-11-11 09:55:41 +0400

    Цикл For задачи  8,9

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+
+// задача 1
+public class Main {
+    public static void main(String[] args) {
+       int sum=0;
+       int save=29000;
+
+        for (int i= 1; i>=12; i++);
+        System.out.println("Месяц " + i + " , сумма накоплений равна " + sum + " рублей");
+        savings= sum+save;
+    }
+}
+
+//задача 2
+
+public class Main{
+    public static void  main(String[] args){
+        int save=29000;
+        int sum=0;
+        for (int i = 1; i <= 12; i++);
+        sum=sum/100;
+        sum=sum+save;
+        System.out.println("Месяц " + i + " , сумма накоплений равна " + sum + " рублей");
+    }
+}
